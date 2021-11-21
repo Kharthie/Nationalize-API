1d843cc49047a2f1dd253ed5b811abfb156d8df3
Kharthie <92292588+Kharthie@users.noreply.github.com>
2021-11-21 13:05:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const api_url = "https://api.nationalize.io/?name=michael";
+// const api_url = "https://api.nationalize.io/?name=michael";
+
+function myFunction() {
+    var x = document.getElementById("myText").value;
+    get(x);
+  }
+  
+  async function get(myFunction) {
+    const data = await fetch(`https://api.nationalize.io/?name=${myFunction}`);
+    const display = await data.json();
+    const show = await create(display);
+    console.log(show);
+  }
+  
+  function create(show) {
+    const para = document.querySelector("#display");
+    para.innerText = `country: ${show.country[0]["country_id"]} => probability: ${show.country[0].probability}
+          country: ${show.country[1]["country_id"]} => probability: ${show.country[1].probability}`;
+    document.body.append(para);
+  }
 
 
 
@@ -14,18 +33,15 @@ const api_url = "https://api.nationalize.io/?name=michael";
  
 
 
-async function contant(){
-    try{
-            var value = await fetch("https://api.nationalize.io/?name=michael")   
-             var fetchvalue = await value.json()
+// async function contant(){
+//     try{
+//             var value = await fetch("https://api.nationalize.io/?name=michael")   
+//              var fetchvalue = await value.json()
         
-             console.log(fetchvalue)
-         }
-    catch(err){
-        console.log(err)
-    }
-}
-contant()
-
-   
-
+//              console.log(fetchvalue)
+//          }
+//     catch(err){
+//         console.log(err)
+//     }
+// }
+// contant()
